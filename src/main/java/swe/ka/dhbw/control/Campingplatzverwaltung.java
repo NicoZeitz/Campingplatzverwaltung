c56c691ebb849e67be0e24f820b54f8cc9deff2b
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2023-06-01 11:46:27 +0200

    real main

--- src/main/java/swe/ka/dhbw/control/Campingplatzverwaltung.java
+++ src/main/java/swe/ka/dhbw/control/Campingplatzverwaltung.java
@@ -1,7 +1,23 @@
 package swe.ka.dhbw.control;
 
 public final class Campingplatzverwaltung {
+    private static Campingplatzverwaltung instance;
+
+    private Campingplatzverwaltung() {}
+
+    public static synchronized Campingplatzverwaltung getInstance() {
+        if(instance == null) {
+            instance = new Campingplatzverwaltung();
+        }
+        return instance;
+    }
+
     public static void main(final String[] args) {
+        var campingplatz = Campingplatzverwaltung.getInstance();
+        campingplatz.startApplication();
+    }
+
+    public void startApplication() {
         System.out.println("Hello World");
     }
 }
