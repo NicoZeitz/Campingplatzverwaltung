dc11dd8ffe57c5d00bfd307857d4c6104ccea312
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2023-06-01 11:24:01 +0200

    added Validator

---
+++ src/main/java/swe/ka/dhbw/model/Person.java
@@ -0,0 +1,70 @@
+package swe.ka.dhbw.model;
+
+import swe.ka.dhbw.util.Validator;
+
+public class Person {
+    public enum Geschlecht {
+        MAENNLICH,
+        WEIBLICH,
+        DIVERS,
+    }
+
+    private String vorname;
+    private String nachname;
+    private Geschlecht geschlecht;
+    private String email;
+    private String telefonnummer;
+
+    public Person(final String vorname, final String nachname, final Geschlecht geschlecht, final String email, final String telefonnummer) {
+        this.setVorname(vorname);
+        this.setNachname(nachname);
+        this.setGeschlecht(geschlecht);
+        this.setEmail(email);
+        this.setTelefonnummer(telefonnummer);
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        Validator.getInstance().validateNotNull(vorname, "Person.vorname");
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        Validator.getInstance().validateNotNull(vorname, "Person.nachname");
+        this.nachname = nachname;
+    }
+
+    public Geschlecht getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setGeschlecht(Geschlecht geschlecht) {
+        Validator.getInstance().validateNotNull(vorname, "Person.geschlecht");
+        this.geschlecht = geschlecht;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        Validator.getInstance().validateEmail(email, "Person.email");
+        this.email = email;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        Validator.getInstance().validateTelefonnummer(telefonnummer, "Person.telefonnummer");
+        this.telefonnummer = telefonnummer;
+    }
+}
---
+++ src/main/java/swe/ka/dhbw/util/Validator.java
@@ -0,0 +1,41 @@
+package swe.ka.dhbw.util;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static Validator instance;
+
+    private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("^\\+(?:[0-9]?){6,14}[0-9]$");
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+
+    private Validator() {}
+
+    public static synchronized Validator getInstance() {
+        if(instance == null) {
+            instance = new Validator();
+        }
+        return instance;
+    }
+
+    public void validateNotNull(final Object object, final String objectName) throws  NullPointerException {
+        if(object == null) {
+            throw  new NullPointerException(objectName + " cannot be null!");
+        }
+    }
+
+    public void validateTelefonnummer(final String phoneNumber, final String phoneNumberName) {
+        var validPhoneNumber = PHONE_NUMBER_PATTERN.matcher(phoneNumber).matches();
+        if(!validPhoneNumber) {
+            throw new IllegalArgumentException(phoneNumberName + " is not a valid phone number!");
+        }
+    }
+
+    public void validateEmail(final String email, final String emailName) {
+        var validEmail = EMAIL_PATTERN.matcher(email).matches();
+        if(!validEmail) {
+            throw new IllegalArgumentException(emailName + " is not a valid email!");
+        }
+    }
+}
