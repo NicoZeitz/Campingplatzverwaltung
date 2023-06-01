2e739d85f4d44f94160aa82e351a26348bc8f584
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2023-06-01 10:39:51 +0200

    initial project

---
+++ src/main/java/swe/ka/dhbw/control/Campingplatzverwaltung.java
@@ -0,0 +1,9 @@
+package swe.ka.dhbw.control;
+
+import de.dhbwka.swe.utils.gui.GUIConstants;
+
+public class Campingplatzverwaltung {
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
